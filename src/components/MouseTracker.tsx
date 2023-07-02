be533ad653254cfcb502c406cd038c32fe0e7106
Adel Sen <adelsenhadjii@gmail.com>
2023-07-02 10:21:02 +0200

    Feat: add mouse movement tracking

---
+++ src/components/MouseTracker.tsx
@@ -0,0 +1,37 @@
+import React, { useRef } from "react";
+
+import { sendRequest } from "../api";
+
+import { useTracking } from "./TrackingProvider";
+
+type Props = {
+  children: React.ReactElement;
+};
+
+const MouseTracker: React.FC<Props> = ({ children }) => {
+  const credentials = useTracking();
+  const mouseAreaRef = useRef<HTMLDivElement>(null);
+
+  const sendEvent = (x: number, y: number) => {
+    console.debug(`mouse position traced on [${x},${y}]`);
+    const data = { x, y, url: location.href };
+    sendRequest("/events/mouse-movements", credentials, data);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (mouseAreaRef.current) {
+      const { clientX, clientY } = e;
+      const { top, left } = mouseAreaRef.current.getBoundingClientRect();
+
+      sendEvent(clientX - left, clientY - top);
+    }
+  };
+
+  return (
+    <div ref={mouseAreaRef} onMouseMove={handleMouseMove} style={{ display: "inline-block" }}>
+      {children}
+    </div>
+  );
+};
+
+export default MouseTracker;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { default as EventTrackerComponent } from "./components/EventTracker";
+import { default as MouseTrackerComponent } from "./components/MouseTracker";
 import { default as TrackingProviderComponent } from "./components/TrackingProvider";
 
 export const EventTracker = EventTrackerComponent;
+export const MouseTracker = MouseTrackerComponent;
 export const TrackingProvider = TrackingProviderComponent;
